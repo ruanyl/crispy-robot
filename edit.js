b7490c8c6464b167bbfb2cf496e7b2d33310c9f2
ruanyl <ruanyu1@gmail.com>
2016-01-24 21:41:39 +0200

    use hash tag to navigate between views

--- edit.js
+++ edit.js
@@ -2,6 +2,7 @@ require('es6-promise').polyfill();
 var MediumEditor = require('medium-editor');
 var MeMarkdown = require('./src/MeMarkdown');
 var fetch = require('isomorphic-fetch');
+var markdown = require("markdown").markdown;
 
 var mdContent = {
   md: ''
@@ -14,7 +15,6 @@ var editor = new MediumEditor(editable, {
   extensions: {
     markdown: new MeMarkdown(function(md) {
       mdContent.md = md;
-      console.log(md);
     })
   }
 });
@@ -31,3 +31,46 @@ saveBtn.addEventListener('click', function(e) {
     body: JSON.stringify(mdContent)
   });
 });
+
+if ("onhashchange" in window) { // event supported?
+  window.onhashchange = function () {
+    hashChanged(window.location.hash);
+  }
+}
+else { // event not supported:
+  var storedHash = window.location.hash;
+  window.setInterval(function () {
+    if (window.location.hash != storedHash) {
+      storedHash = window.location.hash;
+      hashChanged(storedHash);
+    }
+  }, 100);
+}
+
+function hashChanged(hash) {
+  hash = hash.split('/');
+  if(hash[1] === 'view') {
+    toView(hash[2]);
+  } else if(hash[1] === 'edit') {
+    toEdit(hash[2]);
+  }
+}
+
+function toView(id) {
+  document.querySelector('#viewContainer').style.display = 'block';
+  document.querySelector('#editContainer').style.display = 'none';
+
+  fetch('/text.md')
+  .then(function(res) {
+    return res.text();
+  }).then(function(body) {
+    var html = markdown.toHTML(body);
+    console.log(html);
+    document.querySelector('#viewContainer').innerHTML = html;
+  });
+}
+
+function toEdit(id) {
+  document.querySelector('#viewContainer').style.display = 'none';
+  document.querySelector('#editContainer').style.display = 'block';
+}
